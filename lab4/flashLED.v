e87b958fed40757c5c84cc2702b9e3221ff9eff6
Shaform <shaform@gmail.com>
2011-04-23 15:21:01 +0800

    Added special mode for lab4.

--- lab4/flashLED.v
+++ lab4/flashLED.v
@@ -30,47 +30,80 @@ end
 
 always @(posedge clock)
 	if (iSW[0] && ~reset) begin
-		if (iSW[1])
-			case ({oLED, direction})
-				9'b000000010: oLED = 8'b00000010;
-				9'b000000100: oLED = 8'b00000100;
-				9'b000001000: oLED = 8'b00001000;
-				9'b000010000: oLED = 8'b00010000;
-				9'b000100000: oLED = 8'b00100000;
-				9'b001000000: oLED = 8'b01000000;
-				9'b010000000: oLED = 8'b10000000;
-				9'b100000000: {oLED, direction} = 9'b010000001;
-				9'b010000001: oLED = 8'b00100000;
-				9'b001000001: oLED = 8'b00010000;
-				9'b000100001: oLED = 8'b00001000;
-				9'b000010001: oLED = 8'b00000100;
-				9'b000001001: oLED = 8'b00000010;
-				9'b000000101: oLED = 8'b00000001;
-				9'b000000011: {oLED, direction} = 9'b000000100;
-				default:  {oLED, direction} = 9'b000000010;
-			endcase
-		else if (iSW[2])
-			case ({oLED, direction})
-				9'b100000010: oLED = 8'b01000010;
-				9'b010000100: oLED = 8'b00100100;
-				9'b001001000: oLED = 8'b00011000;
-				9'b000110000: {oLED, direction} = 9'b001001001;
-				9'b001001001: oLED = 8'b01000010;
-				9'b010000101: oLED = 8'b10000001;
-				9'b100000011: {oLED, direction} = 9'b010000100;
-				
-				default: {oLED, direction} = 9'b100000010;
-			endcase
-		else if (iSW[3])
-			oLED = oLED + 1;
+		case (iSW[3:1])
+			3'b100:
+				case ({oLED, direction})
+					9'b000000010: oLED = 8'b00000010;
+					9'b000000100: oLED = 8'b00000100;
+					9'b000001000: oLED = 8'b00001000;
+					9'b000010000: oLED = 8'b00010000;
+					9'b000100000: oLED = 8'b00100000;
+					9'b001000000: oLED = 8'b01000000;
+					9'b010000000: oLED = 8'b10000000;
+					9'b100000000: {oLED, direction} = 9'b010000001;
+					9'b010000001: oLED = 8'b00100000;
+					9'b001000001: oLED = 8'b00010000;
+					9'b000100001: oLED = 8'b00001000;
+					9'b000010001: oLED = 8'b00000100;
+					9'b000001001: oLED = 8'b00000010;
+					9'b000000101: oLED = 8'b00000001;
+					9'b000000011: {oLED, direction} = 9'b000000100;
+					default:  {oLED, direction} = 9'b000000010;
+				endcase
+			3'b010:
+				case ({oLED, direction})
+					9'b100000010: oLED = 8'b01000010;
+					9'b010000100: oLED = 8'b00100100;
+					9'b001001000: oLED = 8'b00011000;
+					9'b000110000: {oLED, direction} = 9'b001001001;
+					9'b001001001: oLED = 8'b01000010;
+					9'b010000101: oLED = 8'b10000001;
+					9'b100000011: {oLED, direction} = 9'b010000100;
+
+					default: {oLED, direction} = 9'b100000010;
+				endcase
+			3'b001: oLED = oLED + 1;
+			3'b011:
+				case (oLED)
+					8'b01010101: oLED = 8'b10101010;
+					8'b10101010: oLED = 8'b01010101;
+					default: oLED = 8'b01010101;
+				endcase
+			3'b101:
+				case ({oLED, direction})
+					9'b000000000: oLED = 8'b11111111;
+					9'b111111110: {oLED, direction} = 9'b000000001;
+					9'b000000001: oLED = 8'b01000000;
+					9'b010000001: oLED = 8'b00010000;
+					9'b000100001: oLED = 8'b00000100;
+					9'b000001001: oLED = 8'b00000001;
+					9'b000000011: oLED = 8'b11000000;
+					9'b110000001: oLED = 8'b00110000;
+					9'b001100001: oLED = 8'b00001100;
+					9'b000011001: oLED = 8'b00000011;
+					9'b000000111: oLED = 8'b11100000;
+					9'b111000001: oLED = 8'b00000111;
+					9'b000001111: oLED = 8'b00111100;
+					9'b001111001: {oLED, direction} = 9'b000000000;
+
+					default: {oLED, direction} = 9'b000000000;
+				endcase
+			3'b110: oLED = oLED * 7;
+			3'b111:
+				begin
+					oLED[7:4] = oLED[7:4] - 1;
+					oLED[3:0] = oLED[7:4] * 11;
+				end
+			default: {oLED, direction} = 9'bxxxxxxxxx;
+		endcase
 	end
 	else if (reset) begin
 		oLED = 8'b00000000;
 		direction = 0;
 	end
 
-	assign clock = reset ? iCLK_50
-				: (speed[0] ? (speed[1] ? CLKs[cN-2] : CLKs[cN-1])
-						: (speed[1] ? CLKs[cN] : CLKs[cN-2]));
+assign clock = reset | (~iSW[3] & ~iSW[2] & iSW[1]) ? iCLK_50
+	: (speed[0] ? (speed[1] ? CLKs[cN-2] : CLKs[cN-1])
+	: (speed[1] ? CLKs[cN] : CLKs[cN-2]));
 
-	endmodule
+endmodule
--- lab4/t_counter.v
+++ lab4/t_counter.v
@@ -24,46 +24,74 @@ module t_counter;
 		iCLK_50 = 0;
 		iBTN = 0;
 
-		// Wait 100 ns for global reset to finish
-		#100;
+		// Wait 10 ns for global reset to finish
+		#10;
         
 		// Add stimulus here
 		iBTN[0] = 1;
 		#5;
 		iBTN[0] = 0;
 		#5;
+		$display("switched on.");
 		iSW[0] = 1;
 		#5;
+		$display("mode C on.");
 		iSW[1] = 1;
-		#100;
+		#500;
+		$display("mode B on.");
 		iSW[1] = 0;
 		iSW[2] = 1;
-		#100;
+		#500;
+		$display("mode A on.");
 		iSW[2] = 0;
 		iSW[3] = 1;
 		#100;
+		$display("switched off.");
 		iSW[0] = 0;
 		#50;
+		$display("mode C on.");
 		iSW[3] = 0;
 		iSW[1] = 1;
 		#5;
+		$display("mode A on.");
+		iSW[1] = 0;
+		iSW[3] = 1;
+		$display("switched on.");
 		iSW[0] = 1;
 		#50;
+		$display("speed changed.");
 		iBTN[1] = 1;
 		#5;
 		iBTN[1] = 0;
 		#50;
+		$display("speed changed.");
 		iBTN[1] = 1;
 		#5;
 		iBTN[1] = 0;
 		#50;
+		$display("speed changed.");
 		iBTN[1] = 1;
 		#5;
 		iBTN[1] = 0;
-
+		#50;
+		$display("mode C A on.");
+		iSW[1] = 1;
+		#500;
+		$display("mode C B on.");
+		iSW[3] = 0;
+		iSW[2] = 1;
+		#500;
+		$display("mode B A on.");
+		iSW[1] = 0;
+		iSW[3] = 1;
+		#500;
+		$display("mode C B A on.");
+		iSW[1] = 1;
+		#500;
 	end
 	always #2 iCLK_50 = ~iCLK_50;
 	initial #3000 $finish;
+	initial $monitor("counter: %b", oLED);
       
 endmodule
 
