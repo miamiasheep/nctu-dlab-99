c324934028dff2cd2e07e2624df0b007428f8efc
Shaform <shaform@gmail.com>
2011-04-27 08:42:35 +0800

    Solved synchronized problems in lab4.

--- lab4/edge_detect.v
+++ lab4/edge_detect.v
@@ -1,14 +1,33 @@
 `timescale 1ns / 1ps
 module edge_detect(clock, reset, in, edge_out);
-	input clock, reset, in;
-	output edge_out;
-	reg prev_in;
-	
-	always @(posedge clock)
-		if (reset)
-			prev_in <= 0;
-		else
-			prev_in <= in;
-		
-	assign edge_out = in & !prev_in;
+input clock, reset, in;
+output edge_out;
+reg [1:0] state, next_state;
+
+always @(posedge clock)
+	if (reset)
+		state <= 0;
+	else
+		state <= next_state;
+
+always @(state, in)
+	case (state)
+		2'b01:
+			if (in)
+				next_state = 2'b10;
+			else
+				next_state = 2'b00;
+		2'b10:
+			if (in)
+				next_state = 2'b10;
+			else
+				next_state = 2'b00;
+		default:
+			if (in)
+				next_state = 2'b01;
+			else
+				next_state = 2'b00;
+	endcase
+
+	assign edge_out = (state == 2'b01);
 endmodule
--- lab4/flashLED.v
+++ lab4/flashLED.v
@@ -6,104 +6,135 @@ module flashLED(
 	input in,
 	input reset
 );
-parameter cN = 20;
+parameter cN = 24;
 reg [1:0] speed;
 reg direction;
-reg [cN:0] CLKs;
-wire clock, edge_out;
+reg enable, enable_cnt;
+reg [cN:0] CLKs, CLKs_cnt;
+wire edge_out;
 
 edge_detect ed(iCLK_50, reset, in, edge_out);
 
 always @(posedge iCLK_50)
 begin
-	CLKs = CLKs + 1;
-	if (reset) CLKs = 0;
+	if (reset) begin
+		CLKs <= 0;
+		CLKs_cnt <= 0;
+		enable <= 0;
+	end
+	else begin
+		CLKs <= CLKs + 1;
+		CLKs_cnt <= CLKs_cnt + 1;
+		if (enable)
+			enable <= 0;
+		else if ((speed == 2'b00 && CLKs >= (1 << cN))
+			|| (speed == 2'b01 && CLKs >= (1 << (cN-1)))
+			|| (speed >= 2'b10 && CLKs >= (1 << (cN-2)))) begin
+				enable <= 1;
+				CLKs <= 0;
+		end
+		if (enable_cnt)
+			enable_cnt <= 0;
+		else if (CLKs >= (1 << cN)) begin
+				enable_cnt <= 1;
+				CLKs_cnt <= 0;
+		end
+
+	end
 end
 
 always @(posedge iCLK_50)
 begin
-	if (edge_out)
-		speed = speed - 1;
-	if (speed == 3 || reset)
-		speed = 2'b10;
+	if (reset)
+		speed <= 2'b00;
+	else if (edge_out)
+		case (speed)
+			2'b00:
+				speed <= 2'b01;
+			2'b01:
+				speed <= 2'b10;
+			default:
+				speed <= 2'b00;
+		endcase
 end
 
-always @(posedge clock)
-	if (iSW[0] && ~reset) begin
+always @(posedge iCLK_50)
+	if (reset) begin
+		oLED <= 8'b00000000;
+		direction <= 0;
+	end
+	else if (iSW == 4'b0011)
+		if (iSW[0] && enable_cnt)
+			oLED <= oLED + 1;
+		else
+			oLED <= oLED;
+	else if (iSW[0] && enable) begin
 		case (iSW[3:1])
 			3'b100:
 				case ({oLED, direction})
-					9'b000000010: oLED = 8'b00000010;
-					9'b000000100: oLED = 8'b00000100;
-					9'b000001000: oLED = 8'b00001000;
-					9'b000010000: oLED = 8'b00010000;
-					9'b000100000: oLED = 8'b00100000;
-					9'b001000000: oLED = 8'b01000000;
-					9'b010000000: oLED = 8'b10000000;
-					9'b100000000: {oLED, direction} = 9'b010000001;
-					9'b010000001: oLED = 8'b00100000;
-					9'b001000001: oLED = 8'b00010000;
-					9'b000100001: oLED = 8'b00001000;
-					9'b000010001: oLED = 8'b00000100;
-					9'b000001001: oLED = 8'b00000010;
-					9'b000000101: oLED = 8'b00000001;
-					9'b000000011: {oLED, direction} = 9'b000000100;
-					default:  {oLED, direction} = 9'b000000010;
+					9'b000000010: oLED <= 8'b00000010;
+					9'b000000100: oLED <= 8'b00000100;
+					9'b000001000: oLED <= 8'b00001000;
+					9'b000010000: oLED <= 8'b00010000;
+					9'b000100000: oLED <= 8'b00100000;
+					9'b001000000: oLED <= 8'b01000000;
+					9'b010000000: oLED <= 8'b10000000;
+					9'b100000000: {oLED, direction} <= 9'b010000001;
+					9'b010000001: oLED <= 8'b00100000;
+					9'b001000001: oLED <= 8'b00010000;
+					9'b000100001: oLED <= 8'b00001000;
+					9'b000010001: oLED <= 8'b00000100;
+					9'b000001001: oLED <= 8'b00000010;
+					9'b000000101: oLED <= 8'b00000001;
+					9'b000000011: {oLED, direction} <= 9'b000000100;
+					default:  {oLED, direction} <= 9'b000000010;
 				endcase
 			3'b010:
 				case ({oLED, direction})
-					9'b100000010: oLED = 8'b01000010;
-					9'b010000100: oLED = 8'b00100100;
-					9'b001001000: oLED = 8'b00011000;
-					9'b000110000: {oLED, direction} = 9'b001001001;
-					9'b001001001: oLED = 8'b01000010;
-					9'b010000101: oLED = 8'b10000001;
-					9'b100000011: {oLED, direction} = 9'b010000100;
+					9'b100000010: oLED <= 8'b01000010;
+					9'b010000100: oLED <= 8'b00100100;
+					9'b001001000: oLED <= 8'b00011000;
+					9'b000110000: {oLED, direction} <= 9'b001001001;
+					9'b001001001: oLED <= 8'b01000010;
+					9'b010000101: oLED <= 8'b10000001;
+					9'b100000011: {oLED, direction} <= 9'b010000100;
 
-					default: {oLED, direction} = 9'b100000010;
+					default: {oLED, direction} <= 9'b100000010;
 				endcase
-			3'b001: oLED = oLED + 1;
+			//3'b001: oLED <= oLED + 1;
 			3'b011:
 				case (oLED)
-					8'b01010101: oLED = 8'b10101010;
-					8'b10101010: oLED = 8'b01010101;
-					default: oLED = 8'b01010101;
+					8'b01010101: oLED <= 8'b10101010;
+					8'b10101010: oLED <= 8'b01010101;
+					default: oLED <= 8'b01010101;
 				endcase
 			3'b101:
 				case ({oLED, direction})
-					9'b000000000: oLED = 8'b11111111;
-					9'b111111110: {oLED, direction} = 9'b000000001;
-					9'b000000001: oLED = 8'b01000000;
-					9'b010000001: oLED = 8'b00010000;
-					9'b000100001: oLED = 8'b00000100;
-					9'b000001001: oLED = 8'b00000001;
-					9'b000000011: oLED = 8'b11000000;
-					9'b110000001: oLED = 8'b00110000;
-					9'b001100001: oLED = 8'b00001100;
-					9'b000011001: oLED = 8'b00000011;
-					9'b000000111: oLED = 8'b11100000;
-					9'b111000001: oLED = 8'b00000111;
-					9'b000001111: oLED = 8'b00111100;
-					9'b001111001: {oLED, direction} = 9'b000000000;
+					9'b000000000: oLED <= 8'b11111111;
+					9'b111111110: {oLED, direction} <= 9'b000000001;
+					9'b000000001: oLED <= 8'b01000000;
+					9'b010000001: oLED <= 8'b00010000;
+					9'b000100001: oLED <= 8'b00000100;
+					9'b000001001: oLED <= 8'b00000001;
+					9'b000000011: oLED <= 8'b11000000;
+					9'b110000001: oLED <= 8'b00110000;
+					9'b001100001: oLED <= 8'b00001100;
+					9'b000011001: oLED <= 8'b00000011;
+					9'b000000111: oLED <= 8'b11100000;
+					9'b111000001: oLED <= 8'b00000111;
+					9'b000001111: oLED <= 8'b00111100;
+					9'b001111001: {oLED, direction} <= 9'b000000000;
 
-					default: {oLED, direction} = 9'b000000000;
+					default: {oLED, direction} <= 9'b000000000;
 				endcase
-			3'b110: oLED = oLED * 7;
+			3'b110: oLED <= oLED * 7;
 			3'b111:
 				begin
-					oLED[7:4] = oLED[7:4] - 1;
-					oLED[3:0] = oLED[7:4] * 11;
+					oLED[7:4] <= oLED[7:4] - 1;
+					oLED[3:0] <= oLED[7:4] * 11;
 				end
-			default: {oLED, direction} = 9'bxxxxxxxxx;
+			default: {oLED, direction} <= 9'bxxxxxxxxx;
 		endcase
 	end
-	else if (reset) begin
-		oLED = 8'b00000000;
-		direction = 0;
-	end
-
-assign clock = reset | (~iSW[3] & ~iSW[2] & iSW[1]) ? iCLK_50
-	: (speed[0] ? (speed[1] ? CLKs[cN-2] : CLKs[cN-1])
-	: (speed[1] ? CLKs[cN] : CLKs[cN-2]));
 
 endmodule
