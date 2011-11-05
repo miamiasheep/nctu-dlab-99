1123be3539dda457d9070676f3420b6b7b2d15f7
Shaform <shaform@gmail.com>
2011-11-05 14:17:00 +0800

    Clean up.

--- lab2/or_1b_4.v
+++ lab2/or_1b_4.v
@@ -1,31 +1,12 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:31:49 03/15/2011 
-// Design Name: 
-// Module Name:    or_1b_4 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 module or_1b_4(
-input a, b, c, d,
-output e
-    );
-	 wire W, X;
-	 or_1b or1(a, b, W);
-	 or_1b or2(c, d, X);
-	 or_1b or3(W, X, e);
+	input a, b, c, d,
+	output e
+);
+wire W, X;
+or_1b or1(a, b, W);
+or_1b or2(c, d, X);
+or_1b or3(W, X, e);
 
 
 endmodule
